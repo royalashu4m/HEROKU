121128da1dca3a0e2f9a8934d7887828130f65f9
royalashu4m <84221198+royalashu4m@users.noreply.github.com>
2021-06-29 23:26:02 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 from os import system # chala ja bhai
-system ("git clone https://GitHub.com/LEGENDXOP/MASTERBOT && cd MASTERBOT && python3 -m ULTRA")
+system ("git clone https://GitHub.com/royalashu4m/ROYAlBABA4u && cd ROYALBABA4u && python3 -m ULTRA")
 # try af
 print("OH SYSTEM IS CRASHED")
 system("python3 -m ULTRA")
